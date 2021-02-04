6eb3b91935c79f4865397af743b219a945e78250
Alketa <alketa_1996@hotmail.com>
2021-02-05 01:06:06 +0200

    arrow not functioning

--- src/components/CompleteBox.jsx
+++ src/components/CompleteBox.jsx
@@ -1,24 +1,41 @@
-import React, { Component } from 'react'
-import NineBox from './NineBox'
+import React, { Component } from "react";
+import NineBox from "./NineBox";
 
 export class CompleteBox extends Component {
+  //checking if the sudoku is filled correctly comes here
 
-    //checking if the sudoku is filled correctly comes here
+  //moving with the keyboard
+  handleKeyPress = (event) => {
+    console.log(event.key);
+    //https://www.youtube.com/watch?v=kTzYoZyF45c for help
+  };
 
-    render() {
+  // event listener that listens to the key press - presses anything on the keyboard ITS NOT DETECTING ARROOWWWWSSS!!!
+  // add e.l. only once
+  // unmount the e.l -> take it and add it because its not needed anymore
 
-        const row = []
+  render() {
+    const row = [];
 
-        for(let x = 0; x <=8; x++) {
-            row.push(<NineBox key={x} row = {x} passingValue = {this.props.changeSelected} selected = {this.props.selected} numbValue = {this.props.numbValue}/>)
-        }
-
-        return (
-            <div className ="complete">
-                {row}
-            </div>
-        )
+    for (let x = 0; x <= 8; x++) {
+      row.push(
+        <NineBox
+          key={x}
+          row={x}
+          passingValue={this.props.changeSelected}
+          selected={this.props.selected}
+          numbValue={this.props.numbValue}
+        />
+      );
     }
+
+    return (
+      <div>
+        <div className="complete">{row}</div>
+        <input type="text" id="one" onKeyPress={this.handleKeyPress} />
+      </div>
+    );
+  }
 }
 
-export default CompleteBox
+export default CompleteBox;
